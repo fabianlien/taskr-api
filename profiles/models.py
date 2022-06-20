09d286c5b122d6ede15c7da8e254b44da3d8a859
Fabian Lien <fabian.lien@gmail.com>
2022-06-20 14:12:31 +0000

    add profile serializer and profile list view

--- profiles/models.py
+++ profiles/models.py
@@ -1,1 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=150, blank=True)
+    bio = models.TextField(max_length=300, blank=True)
+    profile_image = models.ImageField(
+        upload_to='images/', default='../default_profile_x3wzpb'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+models.signals.post_save.connect(create_profile, sender=User)
