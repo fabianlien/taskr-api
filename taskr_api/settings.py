0fdbc183054f7b63c9f443fe7e4c5865b1d9da65
Fabian Lien <fabian.lien@gmail.com>
2022-06-21 18:33:04 +0000

    add client origin environs and fix import error in settings.py

--- taskr_api/settings.py
+++ taskr_api/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
 from pathlib import Path
 import os
+import re
 import dj_database_url
+
 
 if os.path.exists('env.py'):
     import env
