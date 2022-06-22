391e14676c938a36a45bce89dc061d46ef88993e
Fabian Lien <fabian.lien@gmail.com>
2022-06-22 14:44:24 +0000

    add task list and detail as generic views, and add urls

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from taskr_api.permissions import IsOwnerOrReadOnly
+from rest_framework import generics, permissions
 from .models import Task
-from .serializers import TaskSerializer
-from rest_framework.views import APIView
+from.serializers import TaskSerializer
 
 
+class TaskList(generics.ListCreateAPIView):
+    """
+    List all tasks from all users
+    """
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Task.objects.order_by('-due_by', '-created_at')
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Display a single task and provide RUD functionality if us is the owner
+    """
+
+    serializer_class = TaskSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Task.objects.order_by('-created_at')
